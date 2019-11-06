0d35da8fa329bf3417791e4a9abbbdd62d9509b1
withyeah <areygim@gmail.com>
2019-11-06 12:18:29 +0900

    :wrench: fix: https available now

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `yerang alex g/kim devlog`,
   author: `Yerang Kim`,
   introduction: `Backend Developer at XAI(CRE Korea). Also Known As Alex Gim. I love Python and Django💞 DONE IS BETTER THAN PERFECT!!! I speak 🇰🇷Korean, 🇬🇧English and little bit of 🇯🇵Japanese.`,
-  siteUrl: `http://yerang.kim`, // Your blog site url
+  siteUrl: `https://yerang.kim`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `withyeah`, // Your GitHub account
